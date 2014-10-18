785e2d9f1fbb8681faf4b6d8129b813ecc8aa5d6
gerdvanhoof <gerd.vanhoof@gmail.com>
2014-10-18 02:30:56 +0200

    edge sense and actions added

--- sucker2000.ino
+++ sucker2000.ino
@@ -1,4 +1,4 @@
-uint32_t measure(uint8_t pin, uint32_t threshold = 3000);
+boolean sensorOnEdge (uint8_t pin);
 void moveWheels(uint32_t velocity, uint8_t dir = HIGH);
 void turnWheels(uint8_t dir = HIGH, uint32_t velocity = 150);
 
@@ -6,6 +6,22 @@ void turnWheels(uint8_t dir = HIGH, uint32_t velocity = 150);
 #define pinDirectionB 13
 #define pinSpeedA 3
 #define pinSpeedB 11
+#define lefEdgeSensor 4             
+#define rightEdgeSensor 5              
+#define edgeSensorThreshold 1000  
+
+#define ACTION_MOVE 0
+#define ACTION_TURN 1
+
+#define TURN_LEFT LOW
+#define TURN_RIGHT HIGH
+#define TURN_DURATION 2000000 // 2 secs
+
+
+uint32_t turnStart;
+byte action;
+byte prevAction;
+byte turnDirection;
 
 void setup()
 {
@@ -14,49 +30,62 @@ void setup()
   pinMode(pinDirectionB, OUTPUT);
   Serial.begin(9600);
   delay(1000);
+  
+  action = ACTION_MOVE;
+  prevAction = ACTION_MOVE;
 }
 
 
 void loop()
 {
-
-// meten  
-//  Serial.println(measure(3), DEC);
-
-  moveWheels(100);
-  delay(1500);
-  
-  stopWheels();
-  delay(500);
-  
-  moveWheels(100, LOW);
-  delay(1000);
-  
-  turnWheels();
-  delay(1000);
-
-  turnWheels(LOW);
-  delay(1000);
-
-}
-
-uint32_t measure(uint8_t pin, uint32_t threshold) 
-{
-  pinMode(pin, OUTPUT);  
-  digitalWrite(pin, HIGH);
-  uint32_t t0 = micros();
-  pinMode(pin, INPUT);
-  
-  while(digitalRead(pin)) {
-    if ((micros() - t0) > threshold) {
-      break;
-    }
+  // check what to do
+  if (sensorOnEdge(lefEdgeSensor)) {
+     Serial.println("LEFT OVER EDGE");
+     action = ACTION_TURN;
+     turnDirection = TURN_RIGHT;
+  } else if (sensorOnEdge(rightEdgeSensor)) {
+     Serial.println("RIGHT OVER EDGE");
+      action = ACTION_TURN;
+      turnDirection = TURN_LEFT;
   }
   
-  uint32_t t1 = micros();
-  return t1 - t0;
+  // start or continue the action
+  switch (action) {
+    
+    case ACTION_MOVE:
+      Serial.println("Moving");
+      moveWheels(100, LOW);
+      break;
+      
+    case ACTION_TURN:
+      // init in case of start turning
+      if (prevAction == ACTION_MOVE) {
+        Serial.println("Start turning");
+        stopWheels();
+        turnStart = micros();
+      }
+      // turn
+      turnWheels(turnDirection);
+      if (turnDirection == TURN_LEFT) {
+        Serial.println("Turning left");
+      } else {
+        Serial.println("Turning right");
+      }
+      
+      // check if we can stop turning
+      uint32_t divTurnTime = micros() - turnStart;
+      //Serial.print("Turned time ");
+      //Serial.println(divTurnTime, DEC);
+      if (divTurnTime > TURN_DURATION) {
+        Serial.println("Stop turning");
+        action = ACTION_MOVE;
+      }
+      break;
+      
+  }
+  prevAction = action;
+ 
 }
-
 
 void moveWheels(uint32_t velocity, uint8_t dir)
 {
@@ -88,4 +117,22 @@ void turnWheels(uint8_t dir, uint32_t velocity)
   analogWrite(pinSpeedB, velocity);
   // XXX variabele tijd maken om de draaihoek te varieren
   delay(500);
+}
+
+// returns true if the sensor is over the edge (for the given edge pin)
+
+boolean sensorOnEdge(uint8_t pin) 
+{
+  pinMode(pin, OUTPUT);  
+  digitalWrite(pin, HIGH);
+  uint32_t t0 = micros();
+  uint32_t dif;
+  pinMode(pin, INPUT);
+  
+  while(digitalRead(pin)) {
+    dif = micros() - t0;
+    if (dif > edgeSensorThreshold) break;
+  }
+  return dif > edgeSensorThreshold;
+
 }
